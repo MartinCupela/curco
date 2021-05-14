f15080a2ca17434744af653cee2f54df2a3fa3a8
martin <martin.cupela@gmail.com>
2021-05-14 11:30:18 +0200

    add configuration for converter API

--- config/default.js
+++ config/default.js
@@ -1,6 +1,14 @@
 module.exports = {
   APP: {
     API_URL: "<APP.API_URL>",
-    SERVER_URL: "<APP.SERVER_URL>"
+    SERVER_PORT: "3000"
+  },
+  API: {
+    URL: "<API.URL>",
+    SERVER_PORT: "3001",
+    API_KEY: "<API.API_KEY>",
+  },
+  ELASTICSEARCH: {
+    HOST: "http://localhost:9200"
   }
 }
