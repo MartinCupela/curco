95b33be6c9709737b43b4925effcd874d175dad5
martin <martin.cupela@gmail.com>
2021-05-17 04:03:12 +0200

    remove REST API

--- packages/services/api/src/routes/currencies.ts
+++
@@ -1,7 +0,0 @@
-import {IncomingMessage, ServerResponse} from "http";
-import currencies from "../mock/currencies";
-
-
-export default async (req: IncomingMessage, res: ServerResponse) => {
-  return currencies;
-}
