2576076e4c2ca9c5ac5411c3445150669f612879
martin <martin.cupela@gmail.com>
2021-05-17 04:16:24 +0200

    add loading and error pages for converter

---
+++ packages/services/app/packages/client/src/pages/Converter/ErrorConverter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorConverter = () => (
+  <div>Error</div>
+);
+
+export default ErrorConverter;
---
+++ packages/services/app/packages/client/src/pages/Converter/LoadingConverter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingConverter = () => (
+  <div>loading</div>
+);
+
+export default LoadingConverter;
