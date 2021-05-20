555bf0f5707db16f38bd6df1a97fcdffc14b0a85
martin <martin.cupela@gmail.com>
2021-05-20 13:46:59 +0200

    normalize Apollo cache by __typename for Conversion type

--- packages/services/app/packages/client/src/services/graphql/client.ts
+++ packages/services/app/packages/client/src/services/graphql/client.ts
@@ -1,7 +1,16 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {ApolloClient, defaultDataIdFromObject, IdGetterObj, InMemoryCache} from "@apollo/client";
+import {StoreObject} from "@apollo/client/utilities";
+
 const client = new ApolloClient({
   uri: `${CONFIG.API.URL}` + "/graph",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    dataIdFromObject: (obj: Readonly<StoreObject>) => {
+      if (obj.__typename === "Conversion") {
+        return obj.__typename
+      }
+      return defaultDataIdFromObject(obj)
+    }
+  })
 });
 
 export default client;
